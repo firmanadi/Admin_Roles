979cdbf04971d907b3c822d30c985ef2f9b7ff6f
firmanadi999 <firman.adi@asclar.com>
2018-08-15 15:10:01 +0700

    Initialization

---
+++ resources/views/admin/permissions/create.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.layout-admin')
+
+@section('title', '| Create Permission')
+
+@section('content')
+
+<div class="content-wrapper">
+  <div class='col-md-12'>
+
+    <h1><i class='fa fa-key'></i> Add Permission</h1>
+    <br>
+
+    {{ Form::open(array('url' => 'admin/permissions')) }}
+
+    <div class="form-group">
+        {{ Form::label('name', 'Name') }}
+        {{ Form::text('name', '', array('class' => 'form-control')) }}
+    </div><br>
+    @if(!$roles->isEmpty()) 
+        <h4>Assign Permission to Roles</h4>
+
+        @foreach ($roles as $role)
+            {{ Form::checkbox('roles[]',  $role->id ) }}
+            {{ Form::label($role->name, ucfirst($role->name)) }}<br>
+
+        @endforeach
+    @endif
+    <br>
+    {{ Form::submit('Add', array('class' => 'btn btn-primary')) }}
+
+    {{ Form::close() }}
+
+</div>
+</div>
+@endsection
---
+++ resources/views/admin/roles/edit.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.layout-admin')
+
+@section('title', '| Edit Role')
+
+@section('content')
+
+<div class="content-wrapper">
+  <div class='col-md-12'>
+    <h1><i class='fa fa-key'></i> Edit Role: {{$role->name}}</h1>
+    <hr>
+
+    {{ Form::model($role, array('route' => array('roles.update', $role->id), 'method' => 'PUT')) }}
+
+    <div class="form-group">
+        {{ Form::label('name', 'Role Name') }}
+        {{ Form::text('name', null, array('class' => 'form-control')) }}
+    </div>
+
+    <h5><b>Assign Permissions</b></h5>
+    @foreach ($permissions as $permission)
+
+        {{Form::checkbox('permissions[]',  $permission->id, $role->permissions ) }}
+        {{Form::label($permission->name, ucfirst($permission->name)) }}<br>
+
+    @endforeach
+    <br>
+    {{ Form::submit('Edit', array('class' => 'btn btn-primary')) }}
+
+    {{ Form::close() }}
+</div>
+</div>
+@endsection
