6a282729ae1a262544915eddc19830460ceddccb
angga hasnal akhir <anggahasnal@gmail.com>
2018-08-15 17:35:37 +0700

    tablenya gajadi
    table blablabla gajadi karna jelek

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,9 +5,6 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-            <table>
-                
-            </table>
                 <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
